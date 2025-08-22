0217feb65c50d65d62fe76d07b8a7b94fd51dbd1
PaidYusuf <yusuf.aslan@std.antalya.edu.tr>
2025-08-22 22:12:54 +0300

    Frontend and backend updates

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
-import 'dotenv/config';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
